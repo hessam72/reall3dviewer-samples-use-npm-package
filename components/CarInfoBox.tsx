070e8bfc2cf0570f4836c5a471e9d71de18a8243
Hesam Tahami <hessamtahani@gmail.com>
2025-07-14 09:44:50 +0330

    working with tailwind css

---
+++ components/CarInfoBox.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { FiInfo, FiChevronDown, FiChevronUp } from "react-icons/fi";
+
+
+
+interface CarInfoProps {
+  carDetails: {
+    manufacturer: string;
+    model: string;
+    price: string;
+    year: string;
+    color: string;
+    fuelType: string;
+    mileage: string;
+    engineCondition: string;
+    chassisCondition: string;
+    bodyCondition: string;
+    insuranceValidity: string;
+    gearbox: string;
+  };
+}
+
+const CarInfoBox: React.FC<CarInfoProps> = ({ carDetails }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className={`carinfo-wrapper fixed bottom-4 right-4 ${isOpen ? 'open_modal' : ''}`}>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex items-center justify-between w-full px-4 py-2 text-white bg-blue-950 rounded-lg shadow-lg hover:bg-blue-900"
+      >
+        <span className="flex items-center gap-2">
+          <FiInfo /> اطلاعات خودرو
+        </span>
+        {isOpen ? <FiChevronUp /> : <FiChevronDown />}
+      </button>
+      <div
+        className={`mt-2 bg-gray-900 shadow-lg rounded-lg p-4 border border-gray-600 transition-all duration-300 ease-in-out transform ${isOpen ? 'max-h-screen opacity-100 scale-100' : 'max-h-0 opacity-0 scale-95'}`}
+        style={{ direction: "rtl", overflow: "hidden" }}
+      >
+        <div className="flex justify-between">
+          <h2 className="text-2xl font-bold mb-4 text-white" >
+            {carDetails.manufacturer} {carDetails.model}
+          </h2>
+          <p className="text-gray-300 text-xs">۲۳ ساعت پیش در تهران، نارمک جنوبی
+          </p>
+
+        </div>
+        <br />
+        <ul className="space-y-2 text-white">
+
+          <div className="flex justify-center gap-8">
+            <div className="flex flex-col justify-center text-center gap-2 b-l">
+              <p className="text-gray-300 text-sm">  <span className="font-semibold">مدل (سال تولید)</span> </p>
+              <h4>{carDetails.year}</h4>
+            </div>
+            <div className="flex flex-col justify-center text-center gap-2 b-l">
+              <p className="text-gray-300 text-sm">  <span className="font-semibold">کارکرد</span> </p>
+              <h4>{carDetails.mileage}</h4>
+            </div>
+            <div className="flex flex-col justify-center text-center gap-2">
+              <p className="text-gray-300 text-sm">  <span className="font-semibold">رنگ</span> </p>
+              <h4>{carDetails.color}</h4>
+            </div>
+          </div>
+          <br />
+          <br />
+
+
+          <li className="w-full flex justify-between px-4 py-2 b-btn">
+            <p className="text-gray-300 text-sm ">نوع سوخت:</p> <p>{carDetails.fuelType}</p>
+          </li>
+          <li className="w-full flex justify-between px-4 py-2 b-btn">
+            <p className="text-gray-300 text-sm ">وضعیت موتور:</p><p> {carDetails.engineCondition}</p>
+          </li>
+          <li className="w-full flex justify-between px-4 py-2 b-btn">
+            <p className="text-gray-300 text-sm ">وضعیت شاسی‌ها:</p><p> {carDetails.chassisCondition}</p>
+          </li>
+          <li className="w-full flex justify-between px-4 py-2 b-btn">
+            <p className="text-gray-300 text-sm ">وضعیت بدنه:</p><p> {carDetails.bodyCondition}</p>
+          </li>
+          <li className="w-full flex justify-between px-4 py-2 b-btn">
+            <p className="text-gray-300 text-sm ">مهلت بیمهٔ شخص ثالث:</p><p> {carDetails.insuranceValidity}</p>
+          </li>
+          <li className="w-full flex justify-between px-4 py-2 b-btn">
+            <p className="text-gray-300 text-sm ">گیربکس:</p><p> {carDetails.gearbox}</p>
+          </li>
+          <li className="w-full flex justify-between px-4 py-2">
+            <p className="text-gray-300 text-sm ">قیمت پایه:</p><p> {carDetails.price}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default CarInfoBox;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
 // pages/index.js
 import dynamic from 'next/dynamic';
 import React from 'react';
+import CarInfoBox from '../components/CarInfoBox';
 
 const Reall3dBrowser = dynamic(() => import('../components/Reall3d'), {
     ssr: false,
 });
+const carDetails = {
+    manufacturer: 'پژو',
+    model: '207',
+    price: '1,200,000 تومان',
+    year: '1403',
+    color: 'سفید صدفی',
+    fuelType: 'بنزینی',
+    mileage: '۱۵٬۰۰۰ کیلومتر',
+    engineCondition: 'سالم',
+    chassisCondition: 'سالم و پلمپ',
+    bodyCondition: 'سالم و بی‌خط و خش',
+    insuranceValidity: '۶ ماه',
+    gearbox: 'اتوماتیک',
+};
 
 export default function Home() {
     return (
         <div style={{ width: '100vw', height: '100vh' }}>
-            <Reall3dBrowser />
             <span style={{ position: 'absolute', color: 'white', top: 10, left: 10 }}>نمای سه بعدی خودروی ۲۰۷</span>
+            <CarInfoBox carDetails={carDetails} />
+            <Reall3dBrowser />
         </div>
     );
 }
